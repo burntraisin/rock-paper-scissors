d2077f3abe3e0a1d762e89f0df26c85a3b3acc58
burntraisin <rbkehan@gmail.com>
2023-01-30 06:43:10 -0500

    Syntax

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 // Computer's choice
-function getComputerChoice () {
+function getComputerChoice() {
     let choicesArray = ["rock", "paper", "scissors"];
     let computerChoice = choicesArray[Math.floor(Math.random() * choicesArray.length)];
     return computerChoice;
+}
+
+function gameRound(playerSelection, computerSelection) {
+    //grab player and computer selection
+    //compare player's selection with computer selection
+    //if player == rock then check if computer == rock paper or scissors
+    //return a string declaring the winner of the round
 }

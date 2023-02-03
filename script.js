6c3892389b4df128855b012ddad82d6fea1a2e4b
burntraisin <rbkehan@gmail.com>
2023-02-03 06:24:24 -0500

    Finish console game

--- script.js
+++ script.js
@@ -12,62 +12,93 @@ function playRound(playerSelection, computerSelection) {
     //if player == rock then check if computer == rock paper or scissors
     if (playerSelectionUpper == "ROCK") {
         if (computerSelectionUpper == "ROCK") {
+            console.log("It's a tie! Rock ties rock.");
             return "It's a tie! Rock ties rock.";
         }
         else if (computerSelectionUpper == "PAPER") {
+            console.log("You lose! Paper beats rock.");
             return "You lose! Paper beats rock.";
         }
         else if (computerSelectionUpper == "SCISSORS") {
+            console.log("You win! Rock beats scissors.");
             return "You win! Rock beats scissors.";
         }
     }
     //if player == paper then check if computer == rock paper or scissors
     if (playerSelectionUpper == "PAPER") {
         if (computerSelectionUpper == "PAPER") {
+            console.log("It's a tie! Paper ties paper.");
             return "It's a tie! Paper ties paper.";
         }
         else if (computerSelectionUpper == "ROCK") {
+            console.log("You win! Paper beats rock.");
             return "You win! Paper beats rock.";
         }
         else if (computerSelectionUpper == "SCISSORS") {
+            console.log("You lose! Scissors beat paper.");
             return "You lose! Scissors beat paper.";
         }
     }
     //if player == scissors then check if computer == rock paper or scissors
     if (playerSelectionUpper == "SCISSORS") {
         if (computerSelectionUpper == "SCISSORS") {
+            console.log("It's a tie! Scissors tie scissors.");
             return "It's a tie! Scissors tie scissors.";
         }
         else if (computerSelectionUpper == "ROCK") {
+            console.log("You lose! Rock beats scissors.");
             return "You lose! Rock beats scissors.";
         }
         else if (computerSelectionUpper == "PAPER") {
+            console.log("You win! Scissors beat paper.");
             return "You win! Scissors beat paper.";
         }
     }
 }
 
-// Get who won in playRound
-function getRoundResult() {
-    let playRoundResult = playRound();
-    if (playRoundResult == "It's a tie! Rock ties rock." || "It's a tie! Paper ties paper." || "It's a tie! Scissors tie scissors.") {
+// Get if the player tied, lost, or won
+function getRoundResult(playerSelection, computerSelection) {
+    let playRoundResult = playRound(playerSelection, computerSelection);
+    if (playRoundResult === "It's a tie! Rock ties rock." || playRoundResult === "It's a tie! Paper ties paper." || playRoundResult === "It's a tie! Scissors tie scissors.") {
         return "TIE";
     }
+    else if (playRoundResult === "You lose! Paper beats rock." || playRoundResult === "You lose! Scissors beat paper." || playRoundResult === "You lose! Rock beats scissors.") {
+        return "LOST";
+    }
+    else if (playRoundResult === "You win! Rock beats scissors." || playRoundResult === "You win! Paper beats rock." || playRoundResult === "You win! Scissors beat paper.") {
+        return "WON";
+    }
 }
-
-const playerSelection = "rOck";
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
 
 function game() {
-    let player;
-    let computer;
-    for (let i = 0; i < 5; i++) {
-        let playRoundResult = getRoundResult();
-        if (playRoundResult == "PLAYER WON") {
+    let player = 0;
+    let computer = 0;
 
+    for (let i = 0; i < 5; i++) {
+        let playerSelection = prompt("Rock, paper, or scissors?");
+        let computerSelection = getComputerChoice();
+        let roundResult = getRoundResult(playerSelection, computerSelection);
+        if (roundResult === "TIE") {
+            player += 1;
+            computer += 1;
+        }
+        else if (roundResult === "LOST") {
+            computer += 1;
+        }
+        else if (roundResult === "WON") {
+            player += 1;
         }
     }
-    //capture results of each round
-    //display who won
+
+    if (player > computer) {
+        console.log("You are the winner!");
+    }
+    else if (player < computer) {
+        console.log("You lost!");
+    }
+    else if (player == computer) {
+        console.log("You tied with the computer!");
+    }
 }
+
+game();
